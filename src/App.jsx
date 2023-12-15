e1e7d204939d087effd681a1112998b192d8c80a
atdeneve <alextdeneve@gmail.com>
2023-12-14 21:01:41 -0700

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
-import { useState } from 'react'
-import {puppyList} from './data.js'
+import { useState } from "react";
+import { puppyList } from "./data.js";
+import "./App.css";
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
-
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
 
   return (
-      <div className="App">
-        {
-          puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>;
-          })
-        }
+
+      <div className="pupCard3">
+        <h1>Welcome to Puppy Pals</h1>
+        <div className="pupCard">
+          {featPupId &&(
+            <div className="pupList">
+              <h2 className="pupame">{featuredPup.name}</h2>
+                <ul>
+                  <li>Age: {featuredPup.name}</li>
+                  <li>Email: {featuredPup.email}</li>
+                </ul>
+            </div>
+          )}
+        {puppies.map((puppy) => {
+            return (
+              <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>
+                {puppy.name}
+              </p>
+            )
+          })}
+        </div>
        </div>
   );
 }
+
+export default App;
 
